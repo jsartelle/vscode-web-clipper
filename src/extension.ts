45574350a53fb7a53aebc4c779f54c4c97ccad82
James Sartelle <james.sartelle@moodmedia.com>
2020-04-08 22:26:39 -0400

    Rebuild with latest extension generator

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as Mercury from '@postlight/mercury-parser';
-import TurndownService from 'turndown';
+import TurndownService = require('turndown');
 import { URL, URLSearchParams } from 'url';
 import * as vscode from 'vscode';
 
--- types/@postlight/mercury-parser.d.ts
+++
@@ -1,3 +0,0 @@
-export const browser: boolean;
-export function fetchResource(url: any): any;
-export function parse(_x: any, ...args: any[]): any;
